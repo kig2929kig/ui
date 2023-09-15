4f00c3d57683c419dfe7a1af42acade4c0a975a1
Ingoo Kang <47412229+kig2929kig@users.noreply.github.com>
2023-09-15 12:48:21 +0900

    Add files via upload

--- game3.py
+++ game3.py
@@ -1,6 +1,15 @@
+import random, base64
 from tkinter import *
 from connectDB import cur, conn
+from io import BytesIO
+from PIL import Image, ImageTk
 
+
+RAND_COUNTRY_LIST = []
+PROBLEM = 0
+
+
+## START 화면 구현 ## 
 def myFrame():
   score_hp_frame = Frame(root)
   score_hp_frame.pack(fill='both')
@@ -48,21 +57,87 @@ def myWidget(score_hp_frame, second_frame, img_frame, btn_frame):
 
   btn1 = Button(btn_frame, text="1번", width=12, height=2, command=btn_click)
   btn1.grid(row=0, column=0, padx=10, pady=6)
-  btn2 = Button(btn_frame, text="2번", width=12, height=2)
+  btn2 = Button(btn_frame, text="2번", width=12, height=2, command=btn_click)
   btn2.grid(row=0, column=1, padx=10, pady=6)
-  btn3 = Button(btn_frame, text="3번", width=12, height=2)
+  btn3 = Button(btn_frame, text="3번", width=12, height=2, command=btn_click)
   btn3.grid(row=1, column=0, padx=10, pady=6)
-  btn4 = Button(btn_frame, text="4번", width=12, height=2)
+  btn4 = Button(btn_frame, text="4번", width=12, height=2, command=btn_click)
   btn4.grid(row=1, column=1, padx=10, pady=6)
+  
+  return img_lbl, btn1, btn2, btn3, btn4
+## END 화면 구현 ##
 
+## START 문제 리스트 ##
+def randCountryList():
+  rnd = random.sample(range(1,50), 10)
+  
+  for r in rnd :
+    sql = f'select * from worldPopulation where 순번 = {r}'
+    cur.execute(sql)
+    RAND_COUNTRY_LIST.append(cur.fetchone())
+## END 문제 리스트 ##  
+
+def showImg(problemNum):
+  sql = f'select * from flag where 순번 = {problemNum}'
+  cur.execute(sql)
+  no, code, img = cur.fetchone()
+  
+  if img == None :
+    pass
+  else :
+    img = base64.b64decode(img)
+    img = Image.open(BytesIO(img))
+    resizedImg = img.resize((200,200))
+    resizedImg = ImageTk.PhotoImage(resizedImg)
+    img_lbl.configure(image=resizedImg)
+    img_lbl.image = resizedImg
+  
 def btn_click() :
-  pass
+  global PROBLEM
+  PROBLEM = PROBLEM + 1
+  showImg(PROBLEM)
+
+def wrongAnser(PROBLEM):
+  answer = RAND_COUNTRY_LIST[PROBLEM][2]
+  wAnser = []
+  for wa in RAND_COUNTRY_LIST:
+    wAnser.append(wa[2])
+  wAnser.remove(answer)
+  
+  btn1_c, btn2_c, btn3_c, btn4_c = random.sample(wAnser,4)
+  btn1.configure(text=btn1_c)
+  btn2.configure(text=btn2_c)
+  btn3.configure(text=btn3_c)
+  btn4.configure(text=btn4_c)
+  
+  answer_num = random.sample(range(1,4),1)
+  print(answer, answer_num)
+  if answer_num[0] == 1 :
+    btn1.configure(text=answer)
+  elif answer_num[0] == 2 :
+    btn2.configure(text=answer)
+  elif answer_num[0] == 3 :
+    btn3.configure(text=answer)
+  elif answer_num[0] == 4 :
+    btn4.configure(text=answer)    
+    
+
+def playGame() :
+  randCountryList()
+  showImg(RAND_COUNTRY_LIST[0][0])
+  wrongAnser(PROBLEM) 
+  
+  
+  
+  
   
 root = Tk()
 root.title('세계 나라별 국기 퀴즈')
 root.geometry('300x400')
 
 score_hp_frame, second_frame, img_frame, btn_frame = myFrame()
-myWidget(score_hp_frame, second_frame, img_frame, btn_frame)
+img_lbl, btn1, btn2, btn3, btn4 = myWidget(score_hp_frame, second_frame, img_frame, btn_frame)
+
+playGame()
 
 root.mainloop()
