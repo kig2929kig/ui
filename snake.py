bac57cf76967318480b44b95e276b078cf3e84ee
Ingoo Kang <47412229+kig2929kig@users.noreply.github.com>
2023-11-30 10:08:51 +0900

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,50 +1,49 @@
 import pygame
 
-# COLORS
-BLUE = (0,0,255) ; RED = (255,0,0) ; WHITE = (255,255,255)
-BLACK = (0,0,0)
-
-# SCREEN SIZE
-SCREEN_WIDTH, SCREEN_HEIGHT = 800,600
 pygame.init()
-dis = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Snake Game by kig2929kig")
-pygame.display.update()
-game_over = False
-
-# FRAME
 clock = pygame.time.Clock()
+#color
+BLUE = (0,0,255); RED = (255,0,0); BLACK = (0,0,0)
+#screen
+SCR_WIDTH, SCR_HEIGHT = 800, 600
+screen = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+pygame.display.set_caption('Snake Game')
+#snake
+snake_size = 30
+snake_pos_x = int(SCR_WIDTH/2 - snake_size/2)
+snake_pos_y = int(SCR_HEIGHT/2 - snake_size/2)
+snake_posx_change = 0
+snake_posy_change = 0
 
-# SNAKE
-x1, y1 = 300,300
-x1_change, y1_change = 0, 0
-
-while not game_over:
+running = True
+while running:
     for event in pygame.event.get():
         #print(event)
         if event.type == pygame.QUIT:
-            game_over = True
+            running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -10
-                y1_change = 0
-            if event.key == pygame.K_RIGHT:
-                x1_change = 10
-                y1_change = 0
             if event.key == pygame.K_UP:
-                x1_change = 0
-                y1_change = -10
+                snake_posx_change = 0
+                snake_posy_change = -10
             if event.key == pygame.K_DOWN:
-                x1_change = 0
-                y1_change = 10
-    
-    x1 += x1_change
-    y1 += y1_change
-    dis.fill(WHITE)            
-    pygame.draw.rect(dis, BLUE, [x1,y1,10,10])
+                snake_posx_change = 0
+                snake_posy_change = 10
+            if event.key == pygame.K_LEFT:
+                snake_posx_change = -10
+                snake_posy_change = 0
+            if event.key == pygame.K_RIGHT:
+                snake_posx_change = 10
+                snake_posy_change = 0
+
+    snake_pos_x += snake_posx_change
+    snake_pos_y += snake_posy_change
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, BLUE, [snake_pos_x,
+                                    snake_pos_y,
+                                    snake_size, snake_size])
     
     pygame.display.update()
-    clock.tick(10)
-pygame.quit()
-quit()
+    clock.tick(30)
+    
 
+pygame.quit()
